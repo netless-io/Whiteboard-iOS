b250a7212c6705569f09b2cad74a3c26aca4496d
leavesster <11785335+leavesster@users.noreply.github.com>
2019-12-16 20:04:13 +0800

    upgrade version

--- Whiteboard/Classes/SDK/WhiteSDK.m
+++ Whiteboard/Classes/SDK/WhiteSDK.m
@@ -22,7 +22,7 @@
 
 + (NSString *)version
 {
-    return @"2.5.0";
+    return @"2.5.1";
 }
 
 - (instancetype)initWithWhiteBoardView:(WhiteBoardView *)boardView config:(WhiteSdkConfiguration *)config commonCallbackDelegate:(nullable id<WhiteCommonCallbackDelegate>)callback
