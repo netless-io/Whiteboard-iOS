0792fdc88177d2bc77959178aa9e00e358d57b7b
leavesster <11785335+leavesster@users.noreply.github.com>
2019-12-20 10:55:00 +0800

    add selector example

--- Example/Whiteboard/RoomCommandListController.m
+++ Example/Whiteboard/RoomCommandListController.m
@@ -35,6 +35,7 @@ typedef NS_ENUM(NSInteger, CommandType) {
     CommandTypeReadonly,
     CommandTypeEnable,
     CommandTypeText,
+    CommandTypeSelector,
     CommandTypePencil,
     CommandTypeEraser,
     CommandTypeRectangle,
@@ -67,7 +68,7 @@ static NSString *kReuseCell = @"reuseCell";
     [super viewDidLoad];
 
     self.commands = @[NSLocalizedString(@"改变布局", nil), NSLocalizedString(@"主播", nil), NSLocalizedString(@"观众", nil), NSLocalizedString(@"移动视角中心", nil), NSLocalizedString(@"移动整体视角", nil), NSLocalizedString(@"当前视角状态", nil), NSLocalizedString(@"发送自定义事件", nil), NSLocalizedString(@"清屏", nil), NSLocalizedString(@"插入新页面", nil), NSLocalizedString(@"插入 PPT", nil), NSLocalizedString(@"插入静态 PPT", nil),
-                      NSLocalizedString(@"插入动态 PPT", nil), NSLocalizedString(@"插入图片", nil), NSLocalizedString(@"获取预览截图", nil), NSLocalizedString(@"获取场景完整封面", nil), NSLocalizedString(@"获取PPT", nil), NSLocalizedString(@"获取页面数据", nil),  NSLocalizedString(@"下一页", nil), NSLocalizedString(@"获取连接状态", nil), NSLocalizedString(@"主动断连", nil), NSLocalizedString(@"视野锁定", nil), NSLocalizedString(@"只读", nil), NSLocalizedString(@"取消只读", nil), NSLocalizedString(@"文本", nil), NSLocalizedString(@"画笔", nil),
+                      NSLocalizedString(@"插入动态 PPT", nil), NSLocalizedString(@"插入图片", nil), NSLocalizedString(@"获取预览截图", nil), NSLocalizedString(@"获取场景完整封面", nil), NSLocalizedString(@"获取PPT", nil), NSLocalizedString(@"获取页面数据", nil),  NSLocalizedString(@"下一页", nil), NSLocalizedString(@"获取连接状态", nil), NSLocalizedString(@"主动断连", nil), NSLocalizedString(@"视野锁定", nil), NSLocalizedString(@"只读", nil), NSLocalizedString(@"取消只读", nil), NSLocalizedString(@"文本", nil), NSLocalizedString(@"选择", nil), NSLocalizedString(@"画笔", nil),
                       NSLocalizedString(@"橡皮擦", nil),
                       NSLocalizedString(@"矩形", nil), NSLocalizedString(@"颜色", nil), NSLocalizedString(@"坐标转换", nil), NSLocalizedString(@"缩放", nil)];
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kReuseCell];
@@ -264,6 +265,13 @@ static NSString *kReuseCell = @"reuseCell";
         case CommandTypeEnable:
             [self.room disableOperations:NO];
             break;
+        case CommandTypeSelector:
+        {
+            WhiteMemberState *mState = [[WhiteMemberState alloc] init];
+            mState.currentApplianceName = ApplianceSelector;
+            [self.room setMemberState:mState];
+            break;
+        }
         case CommandTypeText:
         {
             WhiteMemberState *mState = [[WhiteMemberState alloc] init];
