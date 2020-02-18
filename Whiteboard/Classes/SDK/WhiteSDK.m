2f225338f68eaa404d59bf057b0806d52614ee0f
leavesster <11785335+leavesster@users.noreply.github.com>
2020-02-18 23:30:11 +0800

    2.6.0

--- Whiteboard/Classes/SDK/WhiteSDK.m
+++ Whiteboard/Classes/SDK/WhiteSDK.m
@@ -22,7 +22,7 @@
 
 + (NSString *)version
 {
-    return @"2.5.11";
+    return @"2.6.0";
 }
 
 - (instancetype)initWithWhiteBoardView:(WhiteBoardView *)boardView config:(WhiteSdkConfiguration *)config commonCallbackDelegate:(nullable id<WhiteCommonCallbackDelegate>)callback
