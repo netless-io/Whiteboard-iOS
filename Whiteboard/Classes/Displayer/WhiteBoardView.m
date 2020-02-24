dd3d7e07216abdfc936f6f54347a3419c22e3966
leavesster <11785335+leavesster@users.noreply.github.com>
2020-02-24 12:10:29 +0800

    add source

--- Whiteboard/Classes/Displayer/WhiteBoardView.m
+++ Whiteboard/Classes/Displayer/WhiteBoardView.m
@@ -31,6 +31,7 @@
     if (@available(iOS 10.0.0, *)) {
         configuration.mediaTypesRequiringUserActionForPlayback = WKAudiovisualMediaTypeNone;
 #if defined(__LP64__) && __LP64__
+        //https://mabin004.github.io/2018/06/25/iOS%E5%BA%94%E7%94%A8%E6%B2%99%E7%AE%B1/
         [configuration setValue:@"TRUE" forKey:@"allowUniversalAccessFromFileURLs"];
 #else
         //32位 CPU 支持：https://www.jianshu.com/p/fe876b9d1f7c
