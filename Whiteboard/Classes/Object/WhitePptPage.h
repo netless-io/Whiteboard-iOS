5d719e0d2e9276ff0a11e01f5e55e4f70d313310
leavesster <11785335+leavesster@users.noreply.github.com>
2020-02-28 16:08:12 +0800

    add init API

--- Whiteboard/Classes/Object/WhitePptPage.h
+++ Whiteboard/Classes/Object/WhitePptPage.h
@@ -8,10 +8,17 @@
 #import <UIKit/UIkit.h>
 #import "WhiteObject.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
+
 @interface WhitePptPage : WhiteObject
+
+- (instancetype)initWithSrc:(NSString *)src size:(CGSize)size;
 
 @property (nonatomic, copy) NSString *src;
 @property (nonatomic, assign) CGFloat width;
 @property (nonatomic, assign) CGFloat height;
 
 @end
+
+NS_ASSUME_NONNULL_END
--- Whiteboard/Classes/Object/WhitePptPage.m
+++ Whiteboard/Classes/Object/WhitePptPage.m
@@ -9,6 +9,15 @@
 
 @implementation WhitePptPage
 
+- (instancetype)initWithSrc:(NSString *)src size:(CGSize)size
+{
+    self = [super init];
+    _src = src;
+    _width = size.width;
+    _height = size.height;
+    return self;
+}
+
 + (NSDictionary *)modelCustomPropertyMapper {
     return @{@"src" : @[@"src", @"conversionFileUrl"]};
 }
