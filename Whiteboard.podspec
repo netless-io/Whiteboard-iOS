44b463e48b7632137f8241f6f3dc0b54d8a8a800
leavesster <11785335+leavesster@users.noreply.github.com>
2020-03-25 15:15:54 +0800

    2.7.2

--- Whiteboard.podspec
+++ Whiteboard.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'Whiteboard'
-  s.version          = '2.7.1'
+  s.version          = '2.7.2'
   s.summary          = 'netless.io Whiteboard API on iOS'
 
   s.description      = <<-DESC
--- Whiteboard/Classes/Displayer/WhiteCommonCallbacks.m
+++ Whiteboard/Classes/Displayer/WhiteCommonCallbacks.m
@@ -48,6 +48,21 @@
 {
     NSData *data = [message dataUsingEncoding:NSUTF8StringEncoding];
     NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+    if (dict && [dict[@"name"] isEqualToString:@"pptImageLoadError"]) {
+        NSString *name = dict[@"name"];
+        NSString *notificationName = [NSString stringWithFormat:@"DynamicPpt-%@", name];
+        /**
+         名字
+         name: "pptImageLoadError",
+         加载失败的图片网址
+         src: this.props.imageURL,
+         error 事件
+         event: e,
+         错误类型，是直接从网络加载失败，还是从网络连接加载失败
+         type: this.props.preload ? "indexDB" : "url",
+         */
+        [[NSNotificationCenter defaultCenter] postNotificationName:notificationName object:dict];
+    }
     if (dict && dict[@"shapeId"] && dict[@"mediaType"]) {
         NSString *name = dict[@"action"];
         NSString *notificationName = [NSString stringWithFormat:@"DynamicPpt-%@", name];
