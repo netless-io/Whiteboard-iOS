40fb847df6b3ac03045030c8cb011e2f5d4c12e7
leavesster <11785335+leavesster@users.noreply.github.com>
2020-07-02 15:35:27 +0800

    2.9.7

--- Whiteboard.podspec
+++ Whiteboard.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'Whiteboard'
-  s.version          = '2.9.6'
+  s.version          = '2.9.7'
   s.summary          = 'netless.io Whiteboard API on iOS'
 
   s.description      = <<-DESC
--- Whiteboard/Classes/SDK/WhiteSDK.m
+++ Whiteboard/Classes/SDK/WhiteSDK.m
@@ -22,7 +22,7 @@
 
 + (NSString *)version
 {
-    return @"2.9.6";
+    return @"2.9.7";
 }
 
 - (instancetype)initWithWhiteBoardView:(WhiteBoardView *)boardView config:(WhiteSdkConfiguration *)config commonCallbackDelegate:(nullable id<WhiteCommonCallbackDelegate>)callback
