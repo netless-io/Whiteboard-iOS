0c9c5a41204631f190a9ab699f948b3918584f7a
leavesster <11785335+leavesster@users.noreply.github.com>
2020-07-07 11:24:39 +0800

    add preview URL

--- Whiteboard/Classes/Object/WhitePptPage.h
+++ Whiteboard/Classes/Object/WhitePptPage.h
@@ -14,10 +14,16 @@ NS_ASSUME_NONNULL_BEGIN
 @interface WhitePptPage : WhiteObject
 
 - (instancetype)initWithSrc:(NSString *)src size:(CGSize)size;
+- (instancetype)initWithSrc:(NSString *)src preview:(NSString *)url size:(CGSize)size;
 
 @property (nonatomic, copy) NSString *src;
 @property (nonatomic, assign) CGFloat width;
 @property (nonatomic, assign) CGFloat height;
+
+/**
+ 动态转码ppt，预览截图时，使用的背景图片
+ */
+@property (nonatomic, copy, readonly) NSString *previewURL;
 
 @end
 
--- Whiteboard/Classes/Object/WhitePptPage.m
+++ Whiteboard/Classes/Object/WhitePptPage.m
@@ -7,6 +7,12 @@
 
 #import "WhitePptPage.h"
 
+@interface WhitePptPage ()
+
+@property (nonatomic, copy, readwrite) NSString *previewURL;
+
+@end
+
 @implementation WhitePptPage
 
 - (instancetype)initWithSrc:(NSString *)src size:(CGSize)size
@@ -15,6 +21,13 @@
     _src = src;
     _width = size.width;
     _height = size.height;
+    return self;
+}
+
+- (instancetype)initWithSrc:(NSString *)src preview:(NSString *)url size:(CGSize)size
+{
+    self = [self initWithSrc:src size:size];
+    _previewURL = url;
     return self;
 }
 
