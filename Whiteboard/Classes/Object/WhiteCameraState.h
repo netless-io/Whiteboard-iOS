046fcf8460432e47560689630a5ce36dae96d906
leavesster <11785335+leavesster@users.noreply.github.com>
2021-01-20 10:12:08 +0800

    2.11.12

--- Whiteboard/Classes/Object/WhiteCameraState.h
+++ Whiteboard/Classes/Object/WhiteCameraState.h
@@ -10,7 +10,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 /**
- 描述白板内部坐标系以及视野范围
+ 描述白板内部坐标系,内部坐标系，坐标原点为初始位置的中点，X 轴正方向向右，Y 轴正方向向上。
  @since 2.11.12
  */
 @interface WhiteCameraState : WhiteObject
@@ -18,10 +18,16 @@ NS_ASSUME_NONNULL_BEGIN
 //@property (nonatomic, strong) NSNumber *width;
 //@property (nonatomic, strong) NSNumber *height;
 
-/** 当前 whiteboardView 中点，在 白板内部 坐标系中的坐标 */
+/**
+ 如果房间中没有主播，第一次进入房间，初始位置应为 0
+ 当前 whiteboardView 中点，在 白板内部 坐标系中的坐标
+ */
 @property (nonatomic, strong) NSNumber *centerX;
 
-/** 当前 whiteboardView 中点，在 白板内部 坐标系中的坐标 */
+/**
+ 如果房间中没有主播，第一次进入房间，初始位置应为 0
+ 当前 whiteboardView 中点，在 白板内部 坐标系中的坐标
+ */
 @property (nonatomic, strong) NSNumber *centerY;
 
 /** 白板缩放比例，代表白板中内容被放大缩小的比例（大于 1 时，为放大；小于 1 时，为缩小） */
--- Whiteboard/Classes/SDK/WhiteSDK.m
+++ Whiteboard/Classes/SDK/WhiteSDK.m
@@ -23,7 +23,7 @@
 
 + (NSString *)version
 {
-    return @"2.11.11";
+    return @"2.11.12";
 }
 
 - (instancetype)initWithWhiteBoardView:(WhiteBoardView *)boardView config:(WhiteSdkConfiguration *)config commonCallbackDelegate:(nullable id<WhiteCommonCallbackDelegate>)callback audioMixerBridgeDelegate:( id<WhiteAudioMixerBridgeDelegate>)mixer
