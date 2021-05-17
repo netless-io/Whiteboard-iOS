2101a229e127d5f78fbdb2b983f88933dd878612
leavesster <11785335+leavesster@users.noreply.github.com>
2021-05-17 14:27:35 +0800

    deprecate property

--- Whiteboard/Classes/Object/WhiteScene.h
+++ Whiteboard/Classes/Object/WhiteScene.h
@@ -17,10 +17,9 @@ NS_ASSUME_NONNULL_BEGIN
 /** 通过 WhiteRoom 的 moveScene API 才可以改名。 */
 @property (nonatomic, copy, readonly) NSString *name;
 /**
- 可以通过该属性是否为0，来判断该页面是否有内容。（该数字不计算 ppt，只有 ppt 时，也是0）。
- 该属性，还在试验阶段，不稳定。
+ 
  */
-@property (nonatomic, assign, readonly) NSInteger componentsCount;
+@property (nonatomic, assign, readonly) NSInteger componentsCount __deprecated_msg("this property is always 0");
 @property (nonatomic, strong, readonly, nullable) WhitePptPage *ppt;
 @end
 
