c7d999e5967756312d7dc18221c68a83ab5f3527
leavesster <11785335+leavesster@users.noreply.github.com>
2021-06-04 19:43:07 +0800

    2.12.31

--- Whiteboard/Classes/SDK/WhiteSDK.m
+++ Whiteboard/Classes/SDK/WhiteSDK.m
@@ -23,7 +23,7 @@
 
 + (NSString *)version
 {
-    return @"2.12.30";
+    return @"2.12.31";
 }
 
 - (instancetype)initWithWhiteBoardView:(WhiteBoardView *)boardView config:(WhiteSdkConfiguration *)config commonCallbackDelegate:(nullable id<WhiteCommonCallbackDelegate>)callback audioMixerBridgeDelegate:( id<WhiteAudioMixerBridgeDelegate>)mixer
