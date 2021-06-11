92992decb662d91e2e3325f03152e9271265ba34
leavesster <11785335+leavesster@users.noreply.github.com>
2021-06-11 10:33:13 +0800

    2.13.0

--- Whiteboard.podspec
+++ Whiteboard.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'Whiteboard'
-  s.version          = '2.12.33'
+  s.version          = '2.13.0'
   s.summary          = 'netless.io Whiteboard API on iOS'
 
   s.description      = <<-DESC
--- Whiteboard/Classes/SDK/WhiteSDK.m
+++ Whiteboard/Classes/SDK/WhiteSDK.m
@@ -23,7 +23,7 @@
 
 + (NSString *)version
 {
-    return @"2.12.33";
+    return @"2.13.0";
 }
 
 - (instancetype)initWithWhiteBoardView:(WhiteBoardView *)boardView config:(WhiteSdkConfiguration *)config commonCallbackDelegate:(nullable id<WhiteCommonCallbackDelegate>)callback audioMixerBridgeDelegate:( id<WhiteAudioMixerBridgeDelegate>)mixer
