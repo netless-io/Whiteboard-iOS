55a0113bd57b3cc8fcf8b2a41e0facb1df3437d4
leavesster <11785335+leavesster@users.noreply.github.com>
2021-08-12 13:46:54 +0800

    feat: remove CORS for security

--- Whiteboard/Classes/Displayer/WhiteBoardView.m
+++ Whiteboard/Classes/Displayer/WhiteBoardView.m
@@ -41,13 +41,6 @@
 
     if ([[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion: iOS_10_0_0]) {
         configuration.mediaTypesRequiringUserActionForPlayback = WKAudiovisualMediaTypeNone;
-#if defined(__LP64__) && __LP64__
-        //https://mabin004.github.io/2018/06/25/iOS%E5%BA%94%E7%94%A8%E6%B2%99%E7%AE%B1/
-        [configuration setValue:@"TRUE" forKey:@"allowUniversalAccessFromFileURLs"];
-#else
-        //32位 CPU 支持：https://www.jianshu.com/p/fe876b9d1f7c
-        [configuration setValue:@(1) forKey:@"allowUniversalAccessFromFileURLs"];
-#endif
     }
     
     self = [super initWithFrame:frame configuration:configuration];
