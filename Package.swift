2b2476de32e03c65e8a794cead8738acbe0308ac
xuyunshi <405029644@qq.com>
2022-01-28 16:37:19 +0800

    Declare spm file

---
+++ Package.swift
@@ -0,0 +1,49 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Whiteboard",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(
+            name: "Whiteboard",
+            targets: ["Whiteboard"]),
+    ],
+    dependencies: [
+        .package(name: "DSBridge-IOS", url: "https://github.com/vince-hz/DSBridge-IOS.git", from: .init(3, 1, 0)),
+        .package(name: "YYModel", url: "https://github.com/vince-hz/YYModel.git", from: .init(1, 1, 0))
+    ],
+    targets: [
+        .target(name: "Whiteboard",
+                dependencies: ["YYModel", "DSBridge-IOS"],
+                path: "Whiteboard",
+                exclude: [
+                    "Classes/Model-YYKit",
+                    "Classes/include/cpScript.sh"
+                ],
+                sources: ["Classes"],
+                resources: [
+                    .copy("Resource")
+                ],
+                publicHeadersPath: "Classes/include",
+                cSettings: .headers
+               )
+    ]
+)
+
+extension Array where Element == CSetting {
+    static var headers: [Element] {
+        [
+            .headerSearchPath("Classes/Converter"),
+            .headerSearchPath("Classes/Displayer"),
+            .headerSearchPath("Classes/Model"),
+            .headerSearchPath("Classes/NativeReplayer"),
+            .headerSearchPath("Classes/Object"),
+            .headerSearchPath("Classes/Replayer"),
+            .headerSearchPath("Classes/Room"),
+            .headerSearchPath("Classes/SDK")
+        ]
+    }
+}
