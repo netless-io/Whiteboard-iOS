dbed4bd1200c89834cbf686e5b9c0dd2f8ca9867
xuyunshi <405029644@qq.com>
2022-03-01 15:45:40 +0800

    Bump version to 2.16.5

--- Whiteboard.podspec
+++ Whiteboard.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'Whiteboard'
-  s.version          = '2.16.4'
+  s.version          = '2.16.5'
   s.summary          = 'netless.io Whiteboard API on iOS'
 
   s.description      = <<-DESC
--- Whiteboard/Classes/SDK/WhiteSDK.m
+++ Whiteboard/Classes/SDK/WhiteSDK.m
@@ -23,7 +23,7 @@
 
 + (NSString *)version
 {
-    return @"2.16.4";
+    return @"2.16.5";
 }
 
 - (instancetype)initWithWhiteBoardView:(WhiteBoardView *)boardView config:(WhiteSdkConfiguration *)config commonCallbackDelegate:(nullable id<WhiteCommonCallbackDelegate>)callback audioMixerBridgeDelegate:( id<WhiteAudioMixerBridgeDelegate>)mixer
