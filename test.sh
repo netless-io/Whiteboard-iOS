9df2e4a9a6d3f047b70163c3b7a244ddd5656233
vince <zjxuyunshi@gmail.com>
2022-03-31 16:05:20 +0800

    Add test workflow

--- test.sh
+++ test.sh
@@ -1,8 +1,19 @@
+# outpath
+OUTPATH=$1
 device=`xcrun xctrace list devices 2>&1 | grep -oE 'iPad.*.*Simulator.*1[3-9].*[)]+' | head -1 | grep -oE 'iPad.+?)' | head -1`
 xcodebuild \
   -workspace Example/Whiteboard.xcworkspace \
   -scheme Whiteboard_Tests \
   -sdk iphonesimulator \
-  -destination 'platform=iOS Simulator,name=iPad Air (5th generation)' \
-  test \
-  | xcbeautify
+  -destination "platform=iOS Simulator,name=$device" \
+  test | xcbeautify > $OUTPATH
+  PASSSTR='Tests Passed'
+  TEST_TAIL=$(tail -n 1 $OUTPATH)
+  ISPASS=$(echo $TEST_TAIL | grep "${PASSSTR}")
+  if [[ "$ISPASS" != "" ]]
+  then
+    echo "TEST PASS"
+  else
+    echo "TEST FAIL SEE $OUTPATH"
+    exit 1
+  fi
