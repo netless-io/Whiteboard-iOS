d8d11e0aced1d099fc5e60910881ae3e89f3e8e8
xuyunshi <405029644@qq.com>
2022-07-22 14:41:17 +0800

    Add bridge call command type

---
+++ Whiteboard/Classes/Displayer/WhiteCallBridgeCommand.m
@@ -0,0 +1,12 @@
+//
+//  WhiteCallBridgeCommand.m
+//  Whiteboard
+//
+//  Created by xuyunshi on 2022/7/21.
+//
+
+#import "WhiteCallBridgeCommand.h"
+
+@implementation WhiteCallBridgeCommand
+
+@end
