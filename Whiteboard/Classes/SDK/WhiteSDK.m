b11c1c68a874c36147a591ab57f576a51f2c7bda
xuyunshi <405029644@qq.com>
2023-02-07 14:08:35 +0800

    Bump version to .2.17.0-alpha.19

--- Whiteboard/Classes/SDK/WhiteSDK.m
+++ Whiteboard/Classes/SDK/WhiteSDK.m
@@ -28,7 +28,7 @@
 
 + (NSString *)version
 {
-    return @"2.17.0-alpha.18";
+    return @".2.17.0-alpha.19";
 }
 
 - (instancetype)initWithWhiteBoardView:(WhiteBoardView *)boardView config:(WhiteSdkConfiguration *)config commonCallbackDelegate:(nullable id<WhiteCommonCallbackDelegate>)callback audioMixerBridgeDelegate:( id<WhiteAudioMixerBridgeDelegate>)mixer
