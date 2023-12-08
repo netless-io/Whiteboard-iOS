ef3e154d57070baac3f8feca088214b2e50bf91b
xuyunshi <405029644@qq.com>
2023-12-08 10:54:53 +0800

    Update SPM header

--- Whiteboard/Classes/include/NSObject+YY.h
+++
@@ -1,1 +0,0 @@
-../Model/NSObject+YY.h
--- Whiteboard/Classes/include/cpScript.sh
+++ Whiteboard/Classes/include/cpScript.sh
@@ -1,5 +1,5 @@
 # 先删除当前所有的头文件
-find . -type f -name '*.h' | grep -v Private | xargs -I {} sh -c 'rm {}'
+find . -name '*.h' -exec rm {} \;
 # 再复制新的头文件, 忽略fpa文件夹和ApplePencilDrawHandler
 find .. -type f -name '*.h' | grep -v Private | grep fpa -v | grep ApplePencilDrawHandler -v | xargs -I {} sh -c 'ln -s {} $(basename {})'
 # 有两个WhiteObject, 删掉自动覆盖的，拿Model目录下的内容
