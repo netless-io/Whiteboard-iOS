f45ecd0f05447eb3170ab8dc29768558d35b17db
vince <zjxuyunshi@gmail.com>
2024-07-02 18:17:57 +0800

    Bump version to 2.16.97

--- Whiteboard/Classes/SDK/WhiteSDK.m
+++ Whiteboard/Classes/SDK/WhiteSDK.m
@@ -30,7 +30,7 @@
 
 + (NSString *)version
 {
-    return @"2.16.96";
+    return @"2.16.97";
 }
 
 - (instancetype)initWithWhiteBoardView:(WhiteBoardView *)boardView config:(WhiteSdkConfiguration *)config commonCallbackDelegate:(nullable id<WhiteCommonCallbackDelegate>)callback audioMixerBridgeDelegate:(nullable id<WhiteAudioMixerBridgeDelegate>)mixer effectMixerBridgeDelegate:(nullable id<WhiteAudioEffectMixerBridgeDelegate>)effectMixer {
